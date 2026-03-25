43522803ec7c5dba9d8b393fd5d702bdb67893c8
YeonJung Lee <leeyj0724@cu.ac.kr>
2026-03-25 15:27:02 +0900

    Fix compilation errors and warnings in ex2-11.cpp

---
+++ ex2-11.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    char* ptrArray[2];
+    char** ptrptr;
+    int i;
+
+    ptrArray[0] = "Korea";
+    ptrArray[1] = "Seoul";
+
+    ptrptr = (char**)ptrArray;
+    printf("\n ptrArray[0]의 주소 (&ptrArray[0]) = %p", &ptrArray[0]);
+    printf("\n ptrArray[0]의 값 (ptrArray[0]) = %p",ptrArray[0]);
+    printf("\n ptrArray[0]의 참조값 (*ptrArray[0]) = %c", *ptrArray[0]);
+    printf("\n ptrArray[0]의 참조문자열 (*ptrArray[0]) = %s",ptrArray[0]);
+
+    printf("\n ptrArray[1]의 주소 (&ptrArray[1]) = %p", &ptrArray[1]);
+    printf("\n ptrArray[1]의 값 (ptrArray[1]) = %p",ptrArray[1]);
+    printf("\n ptrArray[1]의 참조값 (*ptrArray[1]) = %c", *ptrArray[1]);
+    printf("\n ptrArray[1]의 참조문자열 (*ptrArray[1]) = %s",*(ptrArray + 1));
+
+    printf("\n ptrptr의 주소 (&ptrptr) = %p", &ptrptr);
+    printf("\n ptrptr의 값 (ptrptr) = %p",ptrptr);
+    printf("\n ptrptr의 1차 참조값 (*ptrptr) = %p", *ptrptr);
+    printf("\n ptrptr의 2차 참조값 (**ptrptr) = %c", **ptrptr);
+    printf("\n ptrptr의 2차 참조문자열 (**ptrptr) = %s",*ptrptr);
+
+    printf("\n\n *ptrArray[0] : ");
+    for (i = 0; i < 5; i++)
+        printf("%c ", *ptrArray[0] + i);
+     printf("\n **ptrptr : ");
+     for (i = 0; i < 5; i++)
+        printf("%c ", *((*ptrptr) + i));
+
+    printf("\n\n *ptrArray[1] : ");
+    for (i = 0; i < 5; i++)
+        printf("%c", *(ptrArray[1] + i));
+    printf("\n **ptrptr + 1 : ");
+    for (i = 0; i < 5; i++)
+        printf("%c", *((*ptrptr) + 1 + i));
+
+    return 0;
+}
