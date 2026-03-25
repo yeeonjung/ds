38183fcb8e932f6564fba10294c0557e268528dd
YeonJung Lee <leeyj0724@cu.ac.kr>
2026-03-25 14:20:53 +0900

    Fix compilation errors: change void main to int main, replace gets with fgets

--- ex2-5.cpp
+++ ex2-5.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     int i, legth = 0;
     char str[50];
     printf("\n문자열을 입력하세요: ");
-    gets(str);
+    fgets(str, sizeof(str), stdin);
     printf("\n입력한 문자열은 \n \"" );
     for (i = 0; str[i]; i++) {
         printf("%c", str[i]);
@@ -13,5 +13,5 @@ void main() {
     printf("\" \n 입니다.");
     printf("\n입력한 문자열의 길이 = %d \n", legth);
 
-    getchar();
+    return 0;
 }
