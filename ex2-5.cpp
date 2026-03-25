97a13342d1a30ec7b1eb486afc8b7bd4a6f5aad8
YeonJung Lee <leeyj0724@cu.ac.kr>
2026-03-25 14:22:44 +0900

    Revert to original code

--- ex2-5.cpp
+++ ex2-5.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main() {
+void main() {
     int i, legth = 0;
     char str[50];
     printf("\n문자열을 입력하세요: ");
-    fgets(str, sizeof(str), stdin);
+    gets(str);
     printf("\n입력한 문자열은 \n \"" );
     for (i = 0; str[i]; i++) {
         printf("%c", str[i]);
@@ -13,5 +13,5 @@ int main() {
     printf("\" \n 입니다.");
     printf("\n입력한 문자열의 길이 = %d \n", legth);
 
-    return 0;
+    getchar();
 }
