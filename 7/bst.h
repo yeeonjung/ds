ce57f6929c43147bc5a49aa07490f93303f1e0fc
YeonJung Lee <leeyj0724@cu.ac.kr>
2026-05-13 14:36:49 +0900

    ex7-4.c

---
+++ 7/bst.h
@@ -0,0 +1,6 @@
+#pragma oncce
+#include "node.h"
+
+treeNode* searchBST(treeNode* root, element x);
+treeNode* insertBSTNode(treeNode* root, element x);
+void deleteBSTNode(treeNode* root, element key);
