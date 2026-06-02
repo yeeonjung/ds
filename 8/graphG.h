39fc00c52f05b118c482fa98b24e9987d9d25aca
YeonJung Lee <leeyj0724@cu.ac.kr>
2026-06-02 23:18:40 +0900

    Create graphG.h

---
+++ 8/graphG.h
@@ -0,0 +1,8 @@
+#pragma once
+int weight[MAX_VERTICES][MAX_VERTICES] = {	
+	{ 0, 10, 5, INF, INF },
+	{ INF, 0, 2, 1, INF },
+	{ INF, 3, 0, 9, 2 },
+	{ INF, INF, INF, 0, 4 },
+	{ 7, INF, INF, 6, 0 },
+};
